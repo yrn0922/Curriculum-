f0854a132322563bd77165b254501e2c6d9b1b2e
yrn0922 <315315@outlook.com>
2024-02-16 16:33:50 +0900

    カリキュラム9

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,12 +13,28 @@
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
+<!-- 以下を追記 -->
+<form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+    @csrf
+    @method('DELETE')
+    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+</form>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
+        <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
     </body>
 </html>
 <a href='/posts/create'>create</a>
+{{ Auth::user()->name }}
